8f421655f422d1f7bd1ff1a0d1092ba7ff5d3724
Daniel Clelland <daniel.clelland@gmail.com>
2016-08-03 20:38:14 +1200

    Strike the earth

---
+++ Ursus/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ursus
+//
+//  Created by Daniel Clelland on 3/08/16.
+//  Copyright © 2016 Daniel Clelland. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
