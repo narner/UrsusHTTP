f460477e725e807ebcd16448d1ecbfccc07b7de2
Daniel Clelland <daniel.clelland@gmail.com>
2016-08-03 21:30:48 +1200

    Singleton boilerplate

---
+++ Ursus/Ursus.swift
@@ -0,0 +1,22 @@
+//
+//  Ursus.swift
+//  Ursus
+//
+//  Created by Daniel Clelland on 3/08/16.
+//  Copyright © 2016 Daniel Clelland. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+struct Ursus {
+    
+    let appl = ""
+    let ship = ""
+    let oryx = ""
+    
+    // MARK: - Singleton
+    
+    static let sharedClient = Ursus()
+    
+}
