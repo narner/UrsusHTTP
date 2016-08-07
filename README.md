c049348a6280ab5bbd8fb9286fa99244a389413d
Daniel Clelland <daniel.clelland@gmail.com>
2016-08-07 15:16:42 +1200

    Logging in works

--- README.md
+++ README.md
@@ -9,3 +9,10 @@ An Urbit client for iOS in Swift.
 - Support for those `{ok: true}` ok/error responses
 - Demo app (which works with `pod Ursus try`)
 - Phonetic base validation (enum + StringLiteralConvertible)
+- Should be able to specify ship/event/desk before a request
+
+- Error cases:
+    - Invalid response
+    - Planet offline
+    - Code incorrect
+    - Phonetic base invalid
--- Ursus/ViewController.swift
+++ Ursus/ViewController.swift
@@ -10,20 +10,80 @@ import UIKit
 import PromiseKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
+    
+    // MARK: Interface outlets
+    
+    @IBOutlet var shipTextField: UITextField!
+    @IBOutlet var codeTextField: UITextField!
+    
+    @IBOutlet var submitButton: UIButton!
+    
+    // MARK: Interface actions
+    
+    @IBAction func submitButtonTapped() {
+        setTextFieldsEnabled(false)
+        authenticate(withShip: shipTextField.text!, andCode: codeTextField.text!)
+    }
+    
+    // MARK: Interface mutation
+    
+    private func setTextFieldsEnabled(enabled: Bool) {
+        shipTextField.enabled = enabled
+        codeTextField.enabled = enabled
+        shipTextField.backgroundColor = enabled ? UIColor.whiteColor() : UIColor.lightGrayColor()
+        codeTextField.backgroundColor = enabled ? UIColor.whiteColor() : UIColor.lightGrayColor()
+    }
+    
+    // MARK: Requests
+    
+    private func authenticate(withShip ship: String, andCode code: String) {
         Ursus.GETAuth().then { auth -> Promise<Auth> in
-            return Ursus.PUTAuth(oryx: auth.oryx!, ship: "hidret-matped", code: "xxx")
-        }.then { auth -> Promise<Auth> in
-            return Ursus.DELETEAuth(oryx: auth.oryx!, ship: "hidret-matped")
+            return Ursus.PUTAuth(oryx: auth.oryx!, ship: ship, code: code)
         }.then { auth in
-            print(auth)
+            self.presentSuccessViewController(auth)
         }.error { error in
-            print(error)
+            self.presentErrorViewController(error as NSError)
         }
+    }
+    
+    // MARK: Alerts
+    
+    private func presentSuccessViewController(auth: Auth) {
+        let viewController = UIAlertController(title: "Success", message: "Logged in as ~\(auth.user!)", preferredStyle: .Alert)
         
+        viewController.addAction(UIAlertAction(title: "OK", style: .Default, handler: { action in
+            self.setTextFieldsEnabled(true)
+        }))
+        
+        presentViewController(viewController, animated: true, completion: nil)
+    }
+    
+    private func presentErrorViewController(error: NSError) {
+        let viewController = UIAlertController(title: "Error", message: error.localizedDescription, preferredStyle: .Alert)
+        
+        viewController.addAction(UIAlertAction(title: "OK", style: .Default, handler: { action in
+            self.setTextFieldsEnabled(true)
+        }))
+            
+        presentViewController(viewController, animated: true, completion: nil)
     }
 
+}
+
+extension ViewController: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        switch textField {
+        case shipTextField:
+            codeTextField.becomeFirstResponder()
+        case codeTextField:
+            setTextFieldsEnabled(false)
+            authenticate(withShip: shipTextField.text!, andCode: codeTextField.text!)
+        default:
+            break
+        }
+        
+        return true
+    }
+    
 }
