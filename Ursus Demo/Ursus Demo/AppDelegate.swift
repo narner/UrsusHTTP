c71d93e85de5e09cc7932dd761d87336e77e2ef0
Daniel Clelland <daniel.clelland@gmail.com>
2020-06-03 14:58:34 +1200

    Set up Ursus class

--- Ursus Demo/Ursus Demo/AppDelegate.swift
+++ Ursus Demo/Ursus Demo/AppDelegate.swift
@@ -7,8 +7,11 @@
 //
 
 import UIKit
+import Ursus
 
 @UIApplicationMain class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    let ursus = Ursus(url: URL(string: "http://192.168.1.65")!, code: "lidlyx-dinmeg-masper-hilbex")
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         return true
