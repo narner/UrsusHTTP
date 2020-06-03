8075a78d8f95b0770af9cfe2536d13657929ea1f
Daniel Clelland <daniel.clelland@gmail.com>
2020-06-03 18:29:51 +1200

    Sketch connect and poke

--- Ursus Demo/Ursus Demo/AppDelegate.swift
+++ Ursus Demo/Ursus Demo/AppDelegate.swift
@@ -18,19 +18,25 @@ import Ursus
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
-//        cancellable = ursus.getAuth()
-//            .map(\.data)
-//            .compactMap { data in
-//                return String(data: data, encoding: .utf8)
-//            }
-//            .sink(
-//                receiveCompletion: { completion in
-//                    print(completion)
-//                },
-//                receiveValue: { value in
-//                    print(value)
-//                }
-//            )
+//        cancellable = ursus.poke(
+//            ship: "habsun-sansep-filfyr-fotpec--simlun-ticrus-matzod-marzod",
+//            app: "chat-store",
+//            mark: "json",
+//            json: ["test": 123]
+//        )
+        cancellable = ursus.connect()
+        .map(\.data)
+        .compactMap { data in
+            return String(data: data, encoding: .utf8)
+        }
+        .sink(
+            receiveCompletion: { completion in
+                print(completion)
+            },
+            receiveValue: { value in
+                print(value)
+            }
+        )
         
         return true
     }
---
+++ Ursus/Poke.swift
@@ -0,0 +1,8 @@
+//
+//  Poke.swift
+//  Pods-Ursus Demo
+//
+//  Created by Daniel Clelland on 3/06/20.
+//
+
+import Foundation
