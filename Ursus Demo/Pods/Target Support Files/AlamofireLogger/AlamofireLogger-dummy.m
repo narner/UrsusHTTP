a2bd06cae2304ce5728fe6b01a61ba0c122bd8b2
Daniel Clelland <daniel.clelland@gmail.com>
2020-06-15 14:54:13 +1200

    Remove demo project

--- Ursus Demo/Pods/Target Support Files/AlamofireLogger/AlamofireLogger-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_AlamofireLogger : NSObject
-@end
-@implementation PodsDummy_AlamofireLogger
-@end
--- Ursus Demo/Pods/Target Support Files/Ursus/Ursus-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Ursus : NSObject
-@end
-@implementation PodsDummy_Ursus
-@end
