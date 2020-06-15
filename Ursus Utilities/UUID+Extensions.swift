3a3a4b7661a30dd3be06592f4c054d9084ec5ce9
Daniel Clelland <daniel.clelland@gmail.com>
2020-06-15 15:47:10 +1200

    Replace UInt128 with BigInt

--- Ursus Utilities/UUID+Extensions.swift
+++ Ursus Utilities/UUID+Extensions.swift
@@ -6,29 +6,29 @@
 //
 
 import Foundation
-import UInt128
+import BigInt
 
 extension UUID {
     
     public var base32String: String {
-        return UInt128(upperBits: upperBits, lowerBits: lowerBits).base32String
+        return BigUInt(words: [upperBits, lowerBits]).base32String
     }
     
 }
 
 extension UUID {
     
-    var upperBits: UInt64 {
+    internal var upperBits: UInt {
         let upperBytes = [uuid.0, uuid.1, uuid.2, uuid.3, uuid.4, uuid.5, uuid.6, uuid.7]
         return upperBytes.reduce(0) { result, byte in
-            return result << 8 + UInt64(byte)
+            return result << 8 + UInt(byte)
         }
     }
     
-    var lowerBits: UInt64 {
+    internal var lowerBits: UInt {
         let lowerBytes = [uuid.8, uuid.9, uuid.10, uuid.11, uuid.12, uuid.13, uuid.14, uuid.15]
         return lowerBytes.reduce(0) { result, byte in
-            return result << 8 + UInt64(byte)
+            return result << 8 + UInt(byte)
         }
     }
     
@@ -36,7 +36,7 @@ extension UUID {
 
 extension BinaryInteger {
     
-    var base32String: String {
+    internal var base32String: String {
         return "0v" + String(self, radix: 32, chunk: 5)
     }
     
@@ -44,7 +44,7 @@ extension BinaryInteger {
 
 extension String {
     
-    init<T: BinaryInteger>(_ value: T, radix: Int = 10, chunk: Int, separator: String = ".") {
+    internal init<T: BinaryInteger>(_ value: T, radix: Int = 10, chunk: Int, separator: String = ".") {
         self = String(String(value, radix: radix).reversed().chunked(by: chunk).joined(separator: separator).reversed())
     }
     
@@ -52,7 +52,7 @@ extension String {
 
 extension Array {
     
-    func chunked(by distance: Int) -> [[Element]] {
+    internal func chunked(by distance: Int) -> [[Element]] {
         return stride(from: startIndex, to: endIndex, by: distance).map { index in
             let newIndex = index.advanced(by: distance) > endIndex ? endIndex : index.advanced(by: distance)
             return Array(self[index..<newIndex])
