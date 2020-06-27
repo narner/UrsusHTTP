f4cf54e55d0d4482db01a002ada8cfd3a98dff01
Daniel Clelland <daniel.clelland@gmail.com>
2020-06-27 16:47:33 +1200

    Clear comments

--- Ursus Utilities/PhoneticBaseObfuscator.swift
+++ Ursus Utilities/PhoneticBaseObfuscator.swift
@@ -93,37 +93,6 @@ extension PhoneticBaseObfuscator {
         return c < k ? c : fe(r, a, b, f, c)
     }
     
-    //-- | 'fe' in B&R (2002).
-    //fe
-    //  :: Int
-    //  -> Word32
-    //  -> Word32
-    //  -> (Int -> Word32 -> Word32)
-    //  -> Word32
-    //  -> Word32
-    //fe r a b f m = loop 1 capL capR where
-    //  capL = m `mod` a
-    //  capR = m `div` a
-    //  loop j !ell !arr
-    //    | j > r =
-    //        if   odd r || arr == a
-    //        then a * arr + ell
-    //        else a * ell + arr
-    //    | otherwise =
-    //        let eff   = f (pred j) arr
-    //            -- NB (jtobin):
-    //            --
-    //            -- note that the "extra" modulo operators here are not redundant as
-    //            -- the addition of ell and eff can (silently) overflow Word32.
-    //            -- modulo p does not distribute over addition, but it does
-    //            -- "distribute modulo p," so this ensures we stay sufficiently
-    //            -- small.
-    //            tmp  = if   odd j
-    //                   then (ell `mod` a + eff `mod` a) `mod` a
-    //                   else (ell `mod` b + eff `mod` b) `mod` b
-    //
-    //        in  loop (succ j) arr tmp
-    
     private static func fe(_ r: Int, _ a: UInt32, _ b: UInt32, _ f: (_ j: Int, _ r: UInt32) -> UInt32, _ m: UInt32) -> UInt32 {
         func loop(_ j: Int, _ ell: UInt32, _ arr: UInt32) -> UInt32 {
             if j > r {
@@ -161,50 +130,6 @@ extension PhoneticBaseObfuscator {
         let c = fen(r, a, b, f, m)
         return c <= k ? c : fen(r, a, b, f, c)
     }
-    
-    //-- | 'fen' in B&R (2002).
-    //fen
-    //  :: Int
-    //  -> Word32
-    //  -> Word32
-    //  -> (Int -> Word32 -> Word32)
-    //  -> Word32
-    //  -> Word32
-    //fen r a b f m = loop r capL capR where
-    //  ahh =
-    //    if   odd r
-    //    then m `div` a
-    //    else m `mod` a
-    //
-    //  ale =
-    //    if   odd r
-    //    then m `mod` a
-    //    else m `div` a
-    //
-    //  capL =
-    //    if   ale == a
-    //    then ahh
-    //    else ale
-    //
-    //  capR =
-    //    if   ale == a
-    //    then ale
-    //    else ahh
-    //
-    //  loop j !ell !arr
-    //    | j < 1     = a * arr + ell
-    //    | otherwise =
-    //        let eff = f (pred j) ell
-    //            -- NB (jtobin):
-    //            --
-    //            -- Slight deviation from B&R (2002) here to prevent negative
-    //            -- values.  We add 'a' or 'b' to arr as appropriate and reduce
-    //            -- 'eff' modulo the same number before performing subtraction.
-    //            --
-    //            tmp = if   odd j
-    //                  then (arr + a - (eff `mod` a)) `mod` a
-    //                  else (arr + b - (eff `mod` b)) `mod` b
-    //        in  loop (pred j) tmp ell
 
     private static func fen(_ r: Int, _ a: UInt32, _ b: UInt32, _ f: (_ j: Int, _ r: UInt32) -> UInt32, _ m: UInt32) -> UInt32 {
         func loop(_ j: Int, ell: UInt32, arr: UInt32) -> UInt32 {
