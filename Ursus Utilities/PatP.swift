ec63866f92a39b57a030004dffc9406acc8b07c4
Daniel Clelland <daniel.clelland@gmail.com>
2020-06-27 16:02:20 +1200

    Move integer boilerplate guff into a protocol

--- Ursus Utilities/PatP.swift
+++ Ursus Utilities/PatP.swift
@@ -8,17 +8,17 @@
 import Foundation
 import BigInt
 
-public struct PatP {
+public struct PatP: WrappedUnsignedInteger {
     
     internal var value: BigUInt
 
     internal init(_ value: BigUInt) {
         self.value = value
     }
+    
+}
 
-    public init() {
-        self.value = .zero
-    }
+extension PatP {
     
     public init(string: String) throws {
         let bytes = try PhoneticBaseParser.parse(string)
@@ -75,14 +75,6 @@ extension PatP: CustomDebugStringConvertible {
     
 }
 
-extension PatP: ExpressibleByIntegerLiteral {
-    
-    public init(integerLiteral value: BigUInt.IntegerLiteralType) {
-        self.init(BigUInt(integerLiteral: value))
-    }
-    
-}
-
 extension PatP: ExpressibleByStringLiteral {
     
     public init(unicodeScalarLiteral value: String.ExtendedGraphemeClusterLiteralType) {
@@ -111,178 +103,4 @@ extension PatP: Codable {
         try container.encode(description)
     }
     
-}
-
-extension PatP: Equatable {
-    
-    public static func == (lhs: PatP, rhs: PatP) -> Bool {
-        return lhs.value == rhs.value
-    }
-    
-}
-
-extension PatP: Comparable {
-    
-    public static func < (lhs: PatP, rhs: PatP) -> Bool {
-        return lhs.value < rhs.value
-    }
-    
-}
-
-extension PatP: Hashable {
-    
-    public func hash(into hasher: inout Hasher) {
-        hasher.combine(value)
-    }
-    
-}
-
-extension PatP: Strideable {
-    
-    public func advanced(by n: BigInt) -> PatP {
-        return PatP(value.advanced(by: n))
-    }
-    
-    public func distance(to other: PatP) -> BigInt {
-        return value.distance(to: other.value)
-    }
-    
-}
-
-extension PatP: UnsignedInteger {
-    
-    public typealias Words = BigUInt.Words
-    
-    public init<T>(_ source: T) where T : BinaryInteger {
-        self.init(BigUInt(source))
-    }
-    
-    public init<T>(_ source: T) where T : BinaryFloatingPoint {
-        self.init(BigUInt(source))
-    }
-    
-    public init?<T>(exactly source: T) where T : BinaryFloatingPoint {
-        guard let value = BigUInt(exactly: source) else {
-            return nil
-        }
-
-        self.init(value)
-    }
-    
-    public init?<T>(exactly source: T) where T : BinaryInteger {
-        guard let value = BigUInt(exactly: source) else {
-            return nil
-        }
-        
-        self.init(value)
-    }
-    
-    public init<T>(clamping source: T) where T : BinaryInteger {
-        self.init(BigUInt(clamping: source))
-    }
-
-    public init<T>(truncatingIfNeeded source: T) where T : BinaryInteger {
-        self.init(BigUInt(truncatingIfNeeded: source))
-    }
-    
-    public var bitWidth: Int {
-        return value.bitWidth
-    }
-    
-    public var trailingZeroBitCount: Int {
-        return value.trailingZeroBitCount
-    }
-    
-    public var words: BigUInt.Words {
-        return value.words
-    }
-    
-    public static var isSigned: Bool {
-        return BigUInt.isSigned
-    }
-    
-    public static func % (lhs: PatP, rhs: PatP) -> PatP {
-        return PatP(lhs.value % rhs.value)
-    }
-    
-    public static func %= (lhs: inout PatP, rhs: PatP) {
-        lhs = lhs % rhs
-    }
-    
-    public static func & (lhs: PatP, rhs: PatP) -> PatP {
-        return PatP(lhs.value & rhs.value)
-    }
-    
-    public static func &= (lhs: inout PatP, rhs: PatP) {
-        lhs = lhs & rhs
-    }
-    
-    public static func * (lhs: PatP, rhs: PatP) -> PatP {
-        return PatP(lhs.value * rhs.value)
-    }
-    
-    public static func *= (lhs: inout PatP, rhs: PatP) {
-        lhs = lhs * rhs
-    }
-    
-    public static func + (lhs: PatP, rhs: PatP) -> PatP {
-        return PatP(lhs.value + rhs.value)
-    }
-    
-    public static func += (lhs: inout PatP, rhs: PatP) {
-        lhs = lhs + rhs
-    }
-    
-    public static func - (lhs: PatP, rhs: PatP) -> PatP {
-        return PatP(lhs.value - rhs.value)
-    }
-    
-    public static func -= (lhs: inout PatP, rhs: PatP) {
-        lhs = lhs - rhs
-    }
-    
-    public static func / (lhs: PatP, rhs: PatP) -> PatP {
-        return PatP(lhs.value / rhs.value)
-    }
-    
-    public static func /= (lhs: inout PatP, rhs: PatP) {
-        lhs = lhs / rhs
-    }
-    
-    public static func << <RHS>(lhs: PatP, rhs: RHS) -> PatP where RHS : BinaryInteger {
-        return PatP(lhs.value << rhs)
-    }
-    
-    public static func <<= <RHS>(lhs: inout PatP, rhs: RHS) where RHS : BinaryInteger {
-        lhs = lhs << rhs
-    }
-    
-    public static func >> <RHS>(lhs: PatP, rhs: RHS) -> PatP where RHS : BinaryInteger {
-        return PatP(lhs.value >> rhs)
-    }
-    
-    public static func >>= <RHS>(lhs: inout PatP, rhs: RHS) where RHS : BinaryInteger {
-        lhs = lhs >> rhs
-    }
-    
-    public static func ^ (lhs: PatP, rhs: PatP) -> PatP {
-        return PatP(lhs.value ^ rhs.value)
-    }
-    
-    public static func ^= (lhs: inout PatP, rhs: PatP) {
-        lhs = lhs ^ rhs
-    }
-    
-    public static func | (lhs: PatP, rhs: PatP) -> PatP {
-        return PatP(lhs.value | rhs.value)
-    }
-    
-    public static func |= (lhs: inout PatP, rhs: PatP) {
-        lhs = lhs | rhs
-    }
-    
-    public static prefix func ~ (x: PatP) -> PatP {
-        return PatP(~x.value)
-    }
-
 }
--- Ursus Utilities/PatQ.swift
+++ Ursus Utilities/PatQ.swift
@@ -8,17 +8,17 @@
 import Foundation
 import BigInt
 
-public struct PatQ {
+public struct PatQ: WrappedUnsignedInteger {
     
     internal var value: BigUInt
 
     internal init(_ value: BigUInt) {
         self.value = value
     }
+    
+}
 
-    public init() {
-        self.value = .zero
-    }
+extension PatQ {
     
     public init(string: String) throws {
         let bytes = try PhoneticBaseParser.parse(string)
@@ -41,14 +41,6 @@ extension PatQ: CustomDebugStringConvertible {
     
     public var debugDescription: String {
         return ".~" + description
-    }
-    
-}
-
-extension PatQ: ExpressibleByIntegerLiteral {
-    
-    public init(integerLiteral value: BigUInt.IntegerLiteralType) {
-        self.init(BigUInt(integerLiteral: value))
     }
     
 }
@@ -81,178 +73,4 @@ extension PatQ: Codable {
         try container.encode(description)
     }
     
-}
-
-extension PatQ: Equatable {
-    
-    public static func == (lhs: PatQ, rhs: PatQ) -> Bool {
-        return lhs.value == rhs.value
-    }
-    
-}
-
-extension PatQ: Comparable {
-    
-    public static func < (lhs: PatQ, rhs: PatQ) -> Bool {
-        return lhs.value < rhs.value
-    }
-    
-}
-
-extension PatQ: Hashable {
-    
-    public func hash(into hasher: inout Hasher) {
-        hasher.combine(value)
-    }
-    
-}
-
-extension PatQ: Strideable {
-    
-    public func advanced(by n: BigInt) -> PatQ {
-        return PatQ(value.advanced(by: n))
-    }
-    
-    public func distance(to other: PatQ) -> BigInt {
-        return value.distance(to: other.value)
-    }
-    
-}
-
-extension PatQ: UnsignedInteger {
-    
-    public typealias Words = BigUInt.Words
-    
-    public init<T>(_ source: T) where T : BinaryInteger {
-        self.init(BigUInt(source))
-    }
-    
-    public init<T>(_ source: T) where T : BinaryFloatingPoint {
-        self.init(BigUInt(source))
-    }
-    
-    public init?<T>(exactly source: T) where T : BinaryFloatingPoint {
-        guard let value = BigUInt(exactly: source) else {
-            return nil
-        }
-
-        self.init(value)
-    }
-    
-    public init?<T>(exactly source: T) where T : BinaryInteger {
-        guard let value = BigUInt(exactly: source) else {
-            return nil
-        }
-        
-        self.init(value)
-    }
-    
-    public init<T>(clamping source: T) where T : BinaryInteger {
-        self.init(BigUInt(clamping: source))
-    }
-
-    public init<T>(truncatingIfNeeded source: T) where T : BinaryInteger {
-        self.init(BigUInt(truncatingIfNeeded: source))
-    }
-    
-    public var bitWidth: Int {
-        return value.bitWidth
-    }
-    
-    public var trailingZeroBitCount: Int {
-        return value.trailingZeroBitCount
-    }
-    
-    public var words: BigUInt.Words {
-        return value.words
-    }
-    
-    public static var isSigned: Bool {
-        return BigUInt.isSigned
-    }
-    
-    public static func % (lhs: PatQ, rhs: PatQ) -> PatQ {
-        return PatQ(lhs.value % rhs.value)
-    }
-    
-    public static func %= (lhs: inout PatQ, rhs: PatQ) {
-        lhs = lhs % rhs
-    }
-    
-    public static func & (lhs: PatQ, rhs: PatQ) -> PatQ {
-        return PatQ(lhs.value & rhs.value)
-    }
-    
-    public static func &= (lhs: inout PatQ, rhs: PatQ) {
-        lhs = lhs & rhs
-    }
-    
-    public static func * (lhs: PatQ, rhs: PatQ) -> PatQ {
-        return PatQ(lhs.value * rhs.value)
-    }
-    
-    public static func *= (lhs: inout PatQ, rhs: PatQ) {
-        lhs = lhs * rhs
-    }
-    
-    public static func + (lhs: PatQ, rhs: PatQ) -> PatQ {
-        return PatQ(lhs.value + rhs.value)
-    }
-    
-    public static func += (lhs: inout PatQ, rhs: PatQ) {
-        lhs = lhs + rhs
-    }
-    
-    public static func - (lhs: PatQ, rhs: PatQ) -> PatQ {
-        return PatQ(lhs.value - rhs.value)
-    }
-    
-    public static func -= (lhs: inout PatQ, rhs: PatQ) {
-        lhs = lhs - rhs
-    }
-    
-    public static func / (lhs: PatQ, rhs: PatQ) -> PatQ {
-        return PatQ(lhs.value / rhs.value)
-    }
-    
-    public static func /= (lhs: inout PatQ, rhs: PatQ) {
-        lhs = lhs / rhs
-    }
-    
-    public static func << <RHS>(lhs: PatQ, rhs: RHS) -> PatQ where RHS : BinaryInteger {
-        return PatQ(lhs.value << rhs)
-    }
-    
-    public static func <<= <RHS>(lhs: inout PatQ, rhs: RHS) where RHS : BinaryInteger {
-        lhs = lhs << rhs
-    }
-    
-    public static func >> <RHS>(lhs: PatQ, rhs: RHS) -> PatQ where RHS : BinaryInteger {
-        return PatQ(lhs.value >> rhs)
-    }
-    
-    public static func >>= <RHS>(lhs: inout PatQ, rhs: RHS) where RHS : BinaryInteger {
-        lhs = lhs >> rhs
-    }
-    
-    public static func ^ (lhs: PatQ, rhs: PatQ) -> PatQ {
-        return PatQ(lhs.value ^ rhs.value)
-    }
-    
-    public static func ^= (lhs: inout PatQ, rhs: PatQ) {
-        lhs = lhs ^ rhs
-    }
-    
-    public static func | (lhs: PatQ, rhs: PatQ) -> PatQ {
-        return PatQ(lhs.value | rhs.value)
-    }
-    
-    public static func |= (lhs: inout PatQ, rhs: PatQ) {
-        lhs = lhs | rhs
-    }
-    
-    public static prefix func ~ (x: PatQ) -> PatQ {
-        return PatQ(~x.value)
-    }
-
 }
